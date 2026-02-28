182fbe55321a619ee96a38b89e839c246be44fae
himanshu-levitation <himanshukawale45@gmail.com>
2026-02-28 11:15:12 +0530

    submitted repos see]

---
+++ .history/data/shortlisted-teams_20260228103539.ts
@@ -0,0 +1,209 @@
+export type ShortlistedTeam = {
+  teamName: string;
+  college: string;
+  members: string[];
+  githubRepo?: string;
+};
+
+export const shortlistedTeams: Record<string, ShortlistedTeam> = {
+  "team-1": {
+    teamName: "codingersCat",
+    college: "Fr. Conceicao Rodrigues College of Engineering (FRCRCE), Bandra",
+    members: ["Ronak", "Omkar", "Kashaan", "Vicky"],
+  },
+  "team-2": {
+    teamName: "overClocked",
+    college: "IIT Madras",
+    members: ["Pratik", "Fulkesh"],
+  },
+  "team-3": {
+    teamName: "Being Notified",
+    college: "IIT Madras",
+    members: ["Harsh", "Vidya", "Naman"],
+  },
+  "team-4": {
+    teamName: "TechNova",
+    college: "CIT Chennai",
+    members: ["Harjeet", "Hema", "Akash Kumar"],
+  },
+  "team-5": {
+    teamName: "thinkbaby",
+    college: "IIIT Naya Raipur",
+    members: ["Keerat", "Chiman", "Gaurav", "Justin"],
+  },
+  "team-6": {
+    teamName: "Four giants",
+    college: "CIT Chennai",
+    members: ["Rishi", "Merwin", "Nishanth", "Akshaya"],
+  },
+  "team-7": {
+    teamName: "BinaryEXE",
+    college: "Ashoka Center of Business and Computer Studies, Nashik",
+    members: ["Mohit", "Anant"],
+  },
+  "team-8": {
+    teamName: "Codelanders",
+    college: "VIT Pune",
+    members: ["Mayur", "Tejas", "Shreyas"],
+  },
+  "team-9": {
+    teamName: "Git Coders",
+    college: "GHRCEMJ",
+    members: ["Manish", "Pranav", "Nishant"],
+  },
+  "team-10": {
+    teamName: "sudo",
+    college: "GHRCEMJ",
+    members: ["Niraj", "Ritesh", "Omkar"],
+  },
+  "team-11": {
+    teamName: "INNOV8",
+    college: "GHRCEMJ",
+    members: ["Riddhi", "Atharv", "Damini", "Bhavesh"],
+  },
+  "team-12": {
+    teamName: "TwinBytes",
+    college: "KK Wagh, Nashik",
+    members: ["Sarthak", "Prachi"],
+  },
+  "team-13": {
+    teamName: "NOVA",
+    college: "KK Wagh, Nashik",
+    members: ["Sakshi", "Venkatesh", "Aditi"],
+  },
+  "team-14": {
+    teamName: "Goggins",
+    college: "GHRCEM Pune",
+    members: ["Arya", "Mansi", "Gopal"],
+  },
+  "team-15": {
+    teamName: "Pretty Dangerous",
+    college: "RC Patel, Shirpur",
+    members: ["Sayali", "Tanisha", "Siddhesh", "Ojaswini"],
+  },
+  "team-16": {
+    teamName: "Tarang",
+    college: "Thakur College, Mumbai",
+    members: ["Sachinkumar", "Rohan"],
+  },
+  "team-17": {
+    teamName: "BitLazy",
+    college: "GHRCEM Pune",
+    members: ["Dnyanesh", "Atharva", "Aum", "Pratik"],
+  },
+  "team-18": {
+    teamName: "Zenith",
+    college: "DY Patil, Pune",
+    members: ["Pranav", "Mohit"],
+  },
+  "team-19": {
+    teamName: "Epic_Failure",
+    college: "KCE Jalgaon",
+    members: ["Jayesh", "Sumit", "Vishal", "Devesh"],
+  },
+  "team-20": {
+    teamName: "Krypto_V",
+    college: "DY Patil, Pune",
+    members: ["Abhishek", "Aditya"],
+  },
+  "team-21": {
+    teamName: "EquiMind",
+    college: "KIT College, Kolhapur",
+    members: ["Sneha", "Vinay", "Shreeya", "Saprem"],
+  },
+  "team-22": {
+    teamName: "Continuum",
+    college: "VIT Pune",
+    members: ["Rajnandini", "Suraj", "Vikrant", "Khushi"],
+  },
+  "team-23": {
+    teamName: "Neon Genesis",
+    college: "SPPU Pune",
+    members: ["Atharva", "Ankush", "Mohit"],
+  },
+  "team-24": {
+    teamName: "Mango",
+    college: "GHRCEM Pune",
+    members: ["Shubham", "Dhruv", "Sujeet", "Sahil"],
+  },
+  "team-25": {
+    teamName: "TechMates",
+    college: "GHRCEMJ",
+    members: ["Yukta", "Upasana", "Hitesh", "Dwarkadhish"],
+  },
+  "team-26": {
+    teamName: "DevKo",
+    college: "SVKM Dhule",
+    members: ["Chetan", "Prerana", "Shruti", "Mayur"],
+  },
+  "team-27": {
+    teamName: "TEAM GitPushers",
+    college: "Sandip University",
+    members: ["Mustafa", "Sumit"],
+  },
+  "team-28": {
+    teamName: "Runtime Rebels",
+    college: "GHRCEM Pune",
+    members: ["Niket", "Sagar", "Aditya"],
+  },
+  "team-29": {
+    teamName: "Arjuna",
+    college: "MIT WPU Pune",
+    members: ["Mokshada", "Jay", "Shubham", "Girish"],
+  },
+  "team-30": {
+    teamName: "semi colon squad",
+    college: "SVKM Dhule",
+    members: ["Rajshri", "Kaveri", "Yogyashri"],
+  },
+  "team-31": {
+    teamName: "Spartans",
+    college: "Pune Vidyarthi Griha, Nashik",
+    members: ["Kanhaiya", "Sagar"],
+  },
+  "team-32": {
+    teamName: "Dev Dominators",
+    college: "PCET's NMIET, Talegaon",
+    members: ["Varun", "Vansh", "Suyog", "Mrugesh"],
+  },
+  "team-33": {
+    teamName: "CtrlAltWin",
+    college: "GHRCEM Pune",
+    members: ["Ayush", "Sumeet", "Swapnil"],
+  },
+  "team-34": {
+    teamName: "Team-Vibeyz",
+    college: "Fr. Conceicao Rodrigues College of Engineering (FRCRCE), Bandra",
+    members: ["Justin", "Bhavesh", "Tuhin", "Shreyas"],
+  },
+  "team-35": {
+    teamName: "#Next_Gen_Rakshak",
+    college: "Parul University",
+    members: ["Vishal", "Jinal", "Prakash"],
+  },
+  "team-36": {
+    teamName: "stack forge",
+    college: "GHRCEMJ",
+    members: ["Samiksha", "Ninad", "Gagan"],
+  },
+  "team-37": {
+    teamName: "ZerothLayer",
+    college: "RC Patel, Shirpur",
+    members: ["Dipak", "Aakanksha", "Purva", "Nihar"],
+  },
+  "team-38": {
+    teamName: "Logic Legion",
+    college: "CSMSS Ch Shahu College of Engineering, Ch Sambhajinagar",
+    members: ["Atharva", "Sakshi", "Yash", "Sukhada"],
+  },
+  "team-39": {
+    teamName: "Merge Point",
+    college: "Raisoni",
+    members: ["Nikita Patil", "Krutika", "Krushna", "Vishakha"],
+  },
+  "team-40": {
+    teamName: "TechXplorers",
+    college: "PICT Pune",
+    members: ["Atharva", "Ayush", "Pranav"],
+  },
+};
